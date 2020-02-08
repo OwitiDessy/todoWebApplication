21fa594e2586ffa6046c723f18d1089210537cff
Dessy Owiti <owitidessy@yahoo.com>
2020-02-08 13:36:59 +0000

    Added a spanDelete attribute

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -19,6 +19,7 @@ $(function() {
 
 
     var spanDelete = document.createElement("span");
+    spanDelete.setAttribute("id", task.id);
 
     // bind the X to a new Javascript function 
     // that new function needs to hit a delete endpoint in the controller
